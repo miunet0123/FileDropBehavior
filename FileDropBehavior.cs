f0688104cea7669c09e228a06d43f929ac67be07
miu <miu@miunet.jp>
2015-03-04 08:46:22 +0900

    First release.

---
+++ FileDropBehavior.cs
@@ -0,0 +1,79 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+
+namespace miunet.Windows.FileDrop
+{
+	public class FileDropBehavior : Behavior<FrameworkElement>
+	{
+		public static readonly DependencyProperty CommandProperty = DependencyProperty.Register(
+			"Command", typeof( ICommand ), typeof( FileDropBehavior ), new PropertyMetadata( default( ICommand ) ) );
+
+		public ICommand Command
+		{
+			get { return (ICommand)GetValue( CommandProperty ); }
+			set { SetValue( CommandProperty, value ); }
+		}
+
+
+		protected override void OnAttached()
+		{
+			base.AssociatedObject.AllowDrop = true;
+			base.AssociatedObject.PreviewDragOver += this.AssociatedObject_PreviewDragOver;
+			base.AssociatedObject.Drop += this.AssociatedObject_Drop;
+			base.OnAttached();
+		}
+
+
+		protected override void OnDetaching()
+		{
+			base.AssociatedObject.PreviewDragOver -= this.AssociatedObject_PreviewDragOver;
+			base.AssociatedObject.Drop -= this.AssociatedObject_Drop;
+			base.OnDetaching();
+		}
+
+
+		private void AssociatedObject_PreviewDragOver( object sender, DragEventArgs e )
+		{
+			if( !e.Data.GetDataPresent( DataFormats.FileDrop, true ) )
+			{
+				e.Effects = DragDropEffects.None;
+				e.Handled = true;
+				return;
+			}
+
+
+			var command = this.Command;
+			if( command == null || !command.CanExecute( e ) )
+			{
+				e.Effects = DragDropEffects.None;
+				e.Handled = true;
+				return;
+			}
+
+			e.Effects = DragDropEffects.Copy;
+			e.Handled = true;
+		}
+
+
+		private void AssociatedObject_Drop( object sender, DragEventArgs e )
+		{
+			if( !e.Data.GetDataPresent( DataFormats.FileDrop, true ) )
+			{
+				return;
+			}
+
+
+			var command = this.Command;
+			if( command == null || !command.CanExecute( e ) )
+			{
+				return;
+			}
+
+			var dropFiles = e.Data.GetData( DataFormats.FileDrop ) as string[];
+			command.Execute( dropFiles );
+			e.Handled = true;
+		}
+	}
+}
---
+++ FileDropCommand.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+using System.Windows.Input;
+
+
+namespace miunet.Windows.FileDrop
+{
+	public class FileDropCommand : ICommand
+	{
+		#region ICommand Members
+
+		public event EventHandler CanExecuteChanged;
+
+		bool ICommand.CanExecute( object parameter )
+		{
+			if( !( parameter is DragEventArgs ) )
+				return false;
+
+			return this.CanExecute( (DragEventArgs)parameter );
+		}
+
+
+		void ICommand.Execute( object parameter )
+		{
+			var files = parameter as string[];
+			if( files == null )
+			{
+				return;
+			}
+
+			this.Execute( files );
+		}
+
+		#endregion
+
+
+		private readonly Predicate<DragEventArgs> _CanExecute;
+		private readonly Action<string[]> _Execute;
+
+		public DragDropEffects DragDropEffects { get; set; }
+
+
+		public FileDropCommand( Action<string[]> execute, Predicate<DragEventArgs> canExecute = null )
+		{
+			this.DragDropEffects = DragDropEffects.Copy;
+
+			if( execute == null ) throw new ArgumentNullException( "execute" );
+
+			_Execute = execute;
+			_CanExecute = canExecute;
+		}
+
+
+		public virtual bool CanExecute( DragEventArgs parameter )
+		{
+			return _CanExecute == null || _CanExecute( parameter );
+		}
+
+
+		public virtual void Execute( string[] parameter )
+		{
+			_Execute( parameter );
+		}
+
+
+		public void RaiseCanExecuteChanged()
+		{
+			var eventHandler = Interlocked.CompareExchange( ref this.CanExecuteChanged, null, null );
+			if( eventHandler != null )
+			{
+				eventHandler( this, new EventArgs() );
+			}
+		}
+	}
+}
